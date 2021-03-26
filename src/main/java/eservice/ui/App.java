ac16c0b0fce6d02735dc9e1198c8f63861aeb6a5
rokymiel <rokymiel@gmail.com>
2021-03-27 00:29:22 +0300

    Добавлен MainPage и JavaFX

---
+++ src/main/java/eservice/ui/App.java
@@ -0,0 +1,24 @@
+package eservice.ui;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/MainPage.fxml"));
+
+        Scene scene = new Scene(root, 1000, 800);
+
+        stage.setTitle("FXML Menu Frame");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
