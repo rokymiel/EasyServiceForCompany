bfb1806f5b96bc4182e92a7f0ab3cde2c2089763
rokymiel <rokymiel@gmail.com>
2021-03-30 23:01:58 +0300

    Добавилена инициализация Firestore

--- src/main/java/eservice/ui/App.java
+++ src/main/java/eservice/ui/App.java
@@ -1,6 +1,9 @@
 package eservice.ui;
 
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +13,15 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .setProjectId("easyservice-e5e67")
+                .build();
+        FirebaseApp.initializeApp(options);
+
+
+
         Parent root = FXMLLoader.load(getClass().getResource("/MainPage.fxml"));
 
         Scene scene = new Scene(root, 1000, 800);
